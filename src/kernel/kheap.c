c83fd5159841a4856bfa2d0123edfbcd89b322f2
wenzapen <329042633@qq.com>
2020-03-26 15:55:26 -0700

    add kheap.c

--- src/kernel/kheap.c
+++ src/kernel/kheap.c
@@ -36,3 +36,135 @@ u32_t kmalloc_p(u32_t size, u32_t* phys) {
 u32_t kmalloc_ap(u32_t size, u32_t* phys) {
     return kmalloc_internal(size, 1, phys);
 }
+
+static s32_t find_smallest_hole(u32_t size, u8_t page_align, heap_t *heap) {
+    int i=0;
+    while(i < heap->index.size) {
+	header_t *header = (header_t *)lookup_ordered_array(i, &heap->index);
+	if(page_align > 0) {
+	    u32_t location = (u32_t)header;
+	    int offset = 0;
+	    if((location+sizeof(header_t)) & 0x00000FFF != 0) {
+		offset = 0x1000 - ((location+sizeof(header_t)) & 0x00000FFF);
+		s32_t hole_size = (s32_t)header->size - offset;
+		if(hole_size >= (s32_t)size)
+		    break;	
+	    } 
+
+	} else if(header->size >= size) {
+	    break;
+	} else {
+	    i++;
+	}
+    }
+    if(i >= heap->index.size) {
+	return -1;
+    }
+    return i;
+}
+
+static s8_t header_t_less_than(void *a, void *b) {
+    return ((header_t *)a)->size < ((header_t *)b)->size ? 1 : 0; 
+}
+heap_t *create_heap(u32_t start, u32_t end, u32_t max, u8_t supervisor, u8_t readonly) {
+    heap_t *heap = (heap_t *)kmalloc(sizeof(heap_t));
+    heap->index = place_ordered_array((void *)start, HEAP_INDEX_SIZE, &header_t_less_than);
+    start += sizeof(type_t) * HEAP_INDEX_SIZE;
+    if(start & 0x00000FFF != 0) {
+	start &= 0xFFFFF000;
+	start += 0x1000;
+    }
+    heap->start_address = start;
+    heap->end_address = end_addr;
+    heap->max_address = max;
+    heap->supervisor = supervisor;
+    heap->readonly = readonly;
+
+    header_t *hole = (header_t *)start;
+    hole->size = end_addr - start;
+    hole->magic = HEAP_MAGIC;
+    hole->is_hole = 1;
+    insert_ordered_array((void *)hole, heap->index);
+    return heap;
+
+}
+
+void *alloc(u32_t size, u8_t page_align, heap_t *heap) {
+    u32_t new_size = size + sizeof(header_t) + sizeof(footer_t);
+    s32_t i = find_smallest_hole(new_size, page_align, heap);
+    if(i == -1) {
+	u32_t old_length = heap->end_address - heap->start_address;
+	u32_t old_end_address = heap->end_address;
+	expand(old_length + new_size, heap);
+	u32_t new_length = heap->end_address - heap->start_address;
+	i = 0;
+	u32_t idx = -1; u32_t value = 0x0;
+	while(i < heap->index.size) {
+	    u32_t tmp = (u32_t)lookup_ordered_array(i, &heap->index);
+	    if(tmp > value) {
+		value = tmp;
+		idx = i;
+	    }
+	    i++;
+	}
+	if(idx == -1) {
+	    header_t *header = (header_t *)old_end_address;
+	    header->magic = HEAP_MAGIC;
+	    header->size = new_length - old_length;
+	    header->is_hole = 1;
+	    foot_t *footer = (footer_t *)(old_end_address+header->size - sizeof(footer_t));
+	    footer->magic = HEAP_MAGIC;
+	    footer->header = header;
+	    insert_ordered_array((void *)header, &heap->index);
+	} else {
+	    header_t *header = lookup_ordered_array(idx, &heap->index);
+	    header->size += new_length - old_length;
+	    footer_t *footer = (footer_t *)((u32_t)header+header->size-sizeof(footer_t));
+	    footer->header = header;
+	    footer->magic = HEAP_MAGIC;
+	}
+
+	return alloc(size, page_align, heap);
+    }
+    header_t *orig_hole_header = (header_t *)lookup_ordered_array(i, &heap->index);
+    u32_t orig_hole_pos = (u32_t)orig_hole_header;
+    u32_t orig_hole_size = orig_hole_header->size;
+    if(orig_hole_size-new_size < sizeof(header_t)+sizeof(footer_t)) {
+	size += orig_hole_size - new_size;
+	new_size = orig_hole_size;
+    }
+    if(page_align && orig_hole_pos & 0x00000FFF) {
+	u32_t new_location = orig_hole_pos + 0x1000 - (orig_hole_pos&0xFFF) - sizeof(header_t);
+	header_t *hole_header = (header_t *)orig_hole_pos;
+	hole_header->size = 0x1000-(orig_hole_pos&0xFFF)-sizeof(header_t);
+	hole_header->magic = HEAP_MAGIC;
+	hole_header->is_hole = 1;
+	footer_t *hole_footer = (footer_t *)((u32_t)new_location - sizeof(footer_t));
+	hole_footer->magic = HEAP_MAGIC;
+	hole_footer->header = hole_header;
+	orig_hole_pos = new_location;
+	orig_hole_size = orig_hole_size - hole_header->size;
+    } else {
+	remove_ordered_array(i, &heap->index);
+    }
+    header_t *block_header = (header_t *)orig_hole_pos;
+    block_header->magic = HEAP_MAGIC;
+    block_header->is_hole = 0;
+    block_header->size = new_size;
+    footer_t *block_footer = (footer_t *)(orig_hole_pos+sizeof(header_t)+size);
+    block_footer->magic = HEAP_MAGIC;
+    block_footer->header = block_header;
+    if(orig_hole_size - new_size > 0) {
+	header_t *hole_header=(header_t *)(orig_hole_pos+sizeof(header_t)+size+sizeof(footer_t));
+	hole_header->magic = HEAP_MAGIC;
+	hole_header->is_hole = 1;
+	hole_header->size = orig_hole_size - new_size;
+	footer_t *hole_footer = (footer_t *)((u32_t)hole_header+orig_hole_size-new_size-sizeof(footer_t));
+	if((u32_t)hole_footer < heap->end_address) {
+	    hole_footer->magic = HEAP_MAGIC;
+	    hole_footer->header = hole_header;
+	}
+	insert_ordered_array((void*)hole_header, &heap->index);
+    }
+    return (void *)((u32_t)block_header+sizeof(header_t));
+}
